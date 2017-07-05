a508838c4c5f602e9245f8c782b0293570590ebd
Administrator <Administrator@STU-27>
2017-07-05 11:22:07 +0800

    吧ggbu

---
+++ zhao/案例/js/header-menu.js
@@ -0,0 +1,10 @@
+$(function(){
+	
+	
+	$('#add').click(function(){
+		console.log("111")
+		$("#popup-menu,#small-triangle").toggleClass("hidden block");
+	})
+	
+	
+})
---
+++ zhao/案例/js/menu.js
@@ -0,0 +1,10 @@
+$(function(){
+	
+	var orign ; 
+	
+	$(".menu-div").click(function(){
+		
+		console.log(orign)
+	});
+	
+})
